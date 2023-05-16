f7f27ff89c9d605ff4af6ec86812cb4f537ec30b
HacobAllon <133789555+HacobAllon@users.noreply.github.com>
2023-05-16 23:53:03 +0800

    Add files via upload
    Added RPLL details

---
+++ js/fr24-airport-overlay-data.js
@@ -0,0 +1,107 @@
+var fr24_airport_overlay_data = [
+	{
+		neBoundLat: 40.665,
+		neBoundLng: -73.75,
+		swBoundLat: 40.6183,
+		swBoundLng: -73.8317,
+		name: "John F Kennedy International Airport",
+		IATA: "JFK",
+		ICAO: "KJFK",
+		uploader: "mkyung",
+		email: "",
+		date: 2014,
+		image: "KJFK.png"
+	},
+	{
+		neBoundLat: 42.0083,
+		neBoundLng: -87.8683,
+		swBoundLat: 41.9583,
+		swBoundLng: -87.9417,
+		name: "Chicago O\'Hare International Airport",
+		IATA: "ORD",
+		ICAO: "KORD",
+		uploader: "mkyung",
+		email: "",
+		date: 2014,
+		image: "KORD.png"
+	},
+	{
+		neBoundLat: 22.3283,
+		neBoundLng: 113.9467,
+		swBoundLat: 22.29,
+		swBoundLng: 113.881777,
+		name: "Hong Kong International Airport",
+		IATA: "HKG",
+		ICAO: "VHHH",
+		uploader: "mkyung",
+		email: "",
+		date: 2010,
+		image: "VHHH_AIP.png"
+	},
+	{
+		neBoundLat: 35.57667,
+		neBoundLng: 139.82333,
+		swBoundLat: 35.52333,
+		swBoundLng: 139.73667,
+		name: "Haneda Airport",
+		IATA: "HND",
+		ICAO: "RJTT",
+		uploader: "mkyung",
+		email: "",
+		date: 2010,
+		image: "RJTT2.png"
+	},	
+	{
+		neBoundLat: 33.6667,
+		neBoundLng: -84.4017,
+		swBoundLat: 33.61667,
+		swBoundLng: -84.4517,
+		name: "Hartsfield-Jackson Atlanta International Airport",
+		IATA: "ATL",
+		ICAO: "KATL",
+		uploader: "mkyung",
+		email: "",
+		date: 2014,
+		image: "KATL-faa2014.png"
+	},
+	{
+		neBoundLat: 51.152333,
+		neBoundLng: -113.9677,
+		swBoundLat: 51.08816,
+		swBoundLng: -114.0643,
+		name: "Calgary International Airport",
+		IATA: "YYC",
+		ICAO: "CYYC",
+		uploader: "mkyung",
+		email: "",
+		date: 2014,
+		image: "CYYC.png"
+	},
+	{
+		neBoundLat: 14.31423,
+		neBoundLng: 121.01571,
+		swBoundLat: 14.29456,
+		swBoundLng: 120.59497,
+		name: "Manila International Airport",
+		IATA: "MNL",
+		ICAO: "RPLL",
+		uploader: "Jacob",
+		email: "",
+		date: 2023,
+		image: "RPLL.png"
+	},	
+	/*
+	{
+		neBoundLat: -33.92273053019009,
+		neBoundLng: 151.20755110740658,
+		swBoundLat: -33.9812189189758,
+		swBoundLng: 151.14664332389827,
+		name: "Sydney Airport",
+		IATA: "SYD",
+		ICAO: "YSSY",
+		uploader: "mkyung",
+		email: "",
+		date: 2014,
+		image: "YSSY.png"
+	},	*/
+	]
