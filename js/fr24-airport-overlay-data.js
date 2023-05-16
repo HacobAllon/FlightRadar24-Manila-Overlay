8728bf50da923625065e29c5b93fc0a48dacf60f
HacobAllon <133789555+HacobAllon@users.noreply.github.com>
2023-05-17 00:16:43 +0800

    Update fr24-airport-overlay-data.js
    fixing the coords for RPLL

--- js/fr24-airport-overlay-data.js
+++ js/fr24-airport-overlay-data.js
@@ -78,10 +78,10 @@ var fr24_airport_overlay_data = [
 		image: "CYYC.png"
 	},
 	{
-		neBoundLat: 14.31423,
-		neBoundLng: 14.29456,
-		swBoundLat: 121.01571,
-		swBoundLng: 120.59497,
+		neBoundLat = 14.528867,
+		neBoundLng = 121.032525,
+		swBoundLat = 14.49545,
+		swBoundLng = 120.99715;
 		name: "Manila International Airport",
 		IATA: "MNL",
 		ICAO: "RPLL",
